d70045ba89d225101a383fc3ca2361472e48f101
Avneet Sharma <95043081+avneetsharma2003@users.noreply.github.com>
2022-10-06 17:38:50 +0530

    Create 337A - Puzzles.cpp

---
+++ 337A - Puzzles.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int f[m];
+    for (int i = 0; i < m; i++) {
+        cin >> f[i];
+    }
+    sort(f, f + m);
+    int ans = f[n - 1] - f[0];
+    for (int i = 1; i <= m - n; i++) {
+        if (f[i + n - 1] - f[i] < ans) {
+            ans = f[i + n - 1] - f[i];
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
