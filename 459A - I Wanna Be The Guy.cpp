91265f4b7eb387a1f200d60993f9769301fc83e7
Avneet Sharma <95043081+avneetsharma2003@users.noreply.github.com>
2022-10-06 15:47:36 +0530

    Create 459A - I Wanna Be The Guy.cpp

---
+++ 459A - I Wanna Be The Guy.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, p, q, arr[200];
+    cin >> n >> p;
+    for (int i =  0 ; i < p ; i++) {
+        cin >> arr[i];
+    }
+    cin >> q;
+    for (int i = p ; i < p + q ; i++){
+        cin >> arr[i];
+    }
+    sort(arr , arr + (p + q));
+    int count = 0;
+    for (int i = 0 ; i < p + q ; i++){
+        if (arr[i] != arr[i + 1]){
+            count++;
+        }
+    }
+    if(n == count){
+        cout << "I become the guy." <<endl;
+    } else {
+        cout << "Oh, my keyboard!" << endl;
+    }
+    return 0;
+}
