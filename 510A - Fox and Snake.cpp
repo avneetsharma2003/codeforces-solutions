b09a599d1c6558b9336b6f28321723c7625ba057
Avneet Sharma <95043081+avneetsharma2003@users.noreply.github.com>
2022-10-07 18:43:46 +0530

    Create 510A - Fox and Snake.cpp

---
+++ 510A - Fox and Snake.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    string str;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        if (i % 2) {
+            str = "";
+            for(int j = 1; j <= m; j++)
+                str += '#';
+        } else if ((i % 2 == 0) && (i % 4 == 0)) {
+            str = "#";
+            for(int j = 2; j <= m; j++)
+                str += '.';
+        } else {
+            str = "";
+            for(int j = 2; j <= m; j++)
+                str += '.';
+            str += '#';
+        }
+        cout << str << endl;
+    }
+    return 0;
+}
